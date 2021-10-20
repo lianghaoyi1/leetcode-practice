e478192c33cfe909c4616cb161138bb59050a095
lianghaoyi1 <73529909+lianghaoyi1@users.noreply.github.com>
2021-10-20 14:52:15 +0800

    Update and rename tree.cpp to tree_Isomorphic.cpp

--- tree_Isomorphic.cpp
+++ tree_Isomorphic.cpp
@@ -26,7 +26,7 @@ R2=Buildtree(T2);
 if (Isomorphic(R1,R2))
 std::cout<<"Yes";
 else
-std::cout<<"NO";
+std::cout<<"No";
 
 return 0;
 }
@@ -35,34 +35,46 @@ int Buildtree(TreeNode T[])
 {
 int N;
 int root;
+
+char element,left,right;
 std::cin>>N;
 int check[N]={};
 for(int i=0;i<N;i++)
 check[i]=0;
 for(int i=0;i<N;i++)
 {
-std::cin>>T[i].Element>>T[i].left>>T[i].right;
-if(T[i].left!='-')
-check[i]=1;   
+std::cin>>element>>left>>right;
+
+T[i].Element=element;
+if(left!='-')
+{
+ T[i].left=left-'0';
+ check[T[i].left]=1;  
+ }
 else
 T[i].left=Null;
 
-if(T[i].right!='-')
-check[i]=1;   
+if(right!='-')
+{   
+T[i].right=right-'0';
+check[T[i].right]=1;
+}
 else
 T[i].right=Null;
 }
 
-for(int i=0;i<N;i++)
+
+for(int i=0;i<N;i++)//有问题
 {
    if (!check[i])
    {
-   int root = i;
+   root = i;
    break;
    }
    else
    root = Null;
 }
+
 return root ;
 }
 
@@ -70,24 +82,19 @@ int Isomorphic(int R1,int R2)
 {
 if((R1==Null) && (R2==Null))  //先考虑是否都为空
   return 1;
-if((R1!=Null)&&(R2==Null) || (R1==Null) && (R2!=Null)) 
+if(((R1!=Null)&&(R2==Null)) || ((R1==Null) && (R2!=Null))) 
   return 0;
 if(T1[R1].Element!=T2[R2].Element)
   return 0;
 if ((T1[R1].left==Null) && (T2[R2].left==Null))
     return Isomorphic(T1[R1].right,T2[R2].right);
 
-if ((T1[R1].left!=Null) && (T2[R2].left!=Null) && T1[R1].Element==T2[R2].Element)
-   return Isomorphic(T1[R1].left,T2[R2].left)&&Isomorphic(T1[R1].right,T2[R2].right);
+if ((T1[R1].left!=Null) && (T2[R2].left!=Null) && (T1[T1[R1].left].Element==T2[T2[R2].left].Element))
+   return (Isomorphic(T1[R1].left,T2[R2].left)&&Isomorphic(T1[R1].right,T2[R2].right));
 else
-    return Isomorphic(T1[R1].left,T2[R2].right)&&Isomorphic(T1[R1].right,T2[R2].left);
+    return (Isomorphic(T1[R1].left,T2[R2].right)&&Isomorphic(T1[R1].right,T2[R2].left));
 
 }
    
-
-
-  
-
-
 
 
